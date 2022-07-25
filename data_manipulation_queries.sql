49462098d6b57ac1b73436436b2dc7d4c16568a4
rongabriel98 <77026454+rongabriel98@users.noreply.github.com>
2022-07-25 16:56:20 -0700

    Update data_manipulation_queries.sql

--- data_manipulation_queries.sql
+++ data_manipulation_queries.sql
@@ -2,20 +2,32 @@
 -- to get all orders for order page
 SELECT order_id, customer_id, order_date_time FROM Orders;
 
+-- to get all orders for order page using aliases and displaying customer name instead of customer id
+SELECT Orders.order_id 'Order ID', Customers.full_name 'Customer Name', Orders.order_date_time 'Time of Order'
+FROM Orders
+INNER JOIN Customers ON Orders.customer_id = Customers.customer_id;
+
 -- to get all customers for customer page
 SELECT customer_id, full_name, phone_number, email FROM Customers;
 
 -- to get all products for product page
-SELECT product_id, product_type_id, ore_id, gem_id, num_gems, product_name, description FROM Products;
+SELECT product_id, product_type_id, ore_id, IFNULL(gem_id, 'None'), num_gems, product_name, IFNULL(description, 'None') FROM Products;
+
+-- to get all products for product page displaying product_type, ore, and gem names instead of their ID
+SELECT Products.product_id 'Product ID', Product_Types.product_type_name 'Product Type', Ore.ore_name 'Ore', IFNULL(Gemstones.gem_name, 'None') AS 'Gem', Products.num_gems 'Gem Quantity', Products.product_name 'Product Name', IFNULL(Products.description, 'None') AS 'Description'
+FROM Products
+INNER JOIN Product_Types ON Products.product_type_id = Product_Types.product_type_id
+INNER JOIN Ore ON Products.ore_id = Ore.ore_id
+INNER JOIN Gemstones ON Products.gem_id = Gemstones.gem_id;
 
 -- to get all product_types for product_type page
-SELECT product_type_id, product_type_price, num_ore_req, description FROM Product_Types;
+SELECT product_type_id, product_type_price, num_ore_req, IFNULL(description, 'None') FROM Product_Types;
 
 -- to get all ore for ore page
-SELECT ore_id, ore_name, price_per_ore, description FROM Ore;
+SELECT ore_id, ore_name, price_per_ore, IFNULL(description, 'None') FROM Ore;
 
 -- to get all gemstones for gem page
-SELECT gem_id, gem_name, gem_price, description FROM Gemstones;
+SELECT gem_id, gem_name, gem_price, IFNULL(description, 'None') FROM Gemstones;
 
 -- used in gem page to search for gems less than a certain price
 SELECT gem_name, gem_price FROM Gemstones WHERE gem_price <= :gem_price_input;
@@ -23,6 +35,12 @@ SELECT gem_name, gem_price FROM Gemstones WHERE gem_price <= :gem_price_input;
 -- select statement for intersection table
 SELECT order_has_product_id, order_id, product_id, total_price FROM Orders_has_Products;
 
+----------------------------------------------------------
+-- select statements for dropdowns utilizing names instead of id
+SELECT product_type_name FROM Product_Types;
+SELECT ore_name FROM Ore;
+SELECT gem_name FROM Gemstones;
+SELECT full_name from Customers;
 
 -------------------------
 --INSERTS for the entity tables
